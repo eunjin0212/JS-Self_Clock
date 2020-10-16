c6f92cfd20cec2542bc8024a208efff37954e380
eunjin0212 <eunjin9639@gmail.com>
2020-10-16 18:08:53 +0900

    clockCss

--- clock.js
+++ clock.js
@@ -1,27 +1,7 @@
-const app = document.getElementById("app");
-function clockDraw() {
-  $("#app").width("300px");
-  $("#app").height("300px");
-  const creatText = app.getContext("2d");
-  $("#app").css("backgroundColor", "white");
-  const radius = $("#app").css("borderRadius", "50%");
-  $("#app").css("border", "2px solid pink");
+function setNumPosition() {
+  const nums = document.querySelectorAll(".number");
+  for (let j = 0; i < nums.length; i++) {
+    if (j + 1 === 3 || j + 1 === 6 || j + 1 === 9 || j + 1 === 12) continue;
+    const angle = (j + 1) * (Math.PI / 6);
+  }
 }
-for (let i = 0; i < 12; i++) {
-  const number = document.createElement("div");
-  app.appendChild(number);
-  number.innerHTML = i + 1;
-  number.className = "number" + (i + 1);
-}
-const center = document.createElement("div");
-app.appendChild(center);
-center.className = "center";
-const hour = document.createElement("div");
-app.appendChild(hour);
-hour.className = "hour";
-const minute = document.createElement("div");
-app.appendChild(minute);
-minute.className = "minute";
-const second = document.createElement("div");
-app.appendChild(second);
-second.className = "second";
