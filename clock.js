c932aa87023c5759fc5ee6e544475fb9685671c4
eunjin0212 <eunjin9639@gmail.com>
2020-10-19 19:14:58 +0900

    notJquery

--- clock.js
+++ clock.js
@@ -1,7 +1,6 @@
 const clock = document.querySelector("#clock");
 const body = document.querySelector("body");
 
-setInterval(updateClock, 1000);
 //number
 for (let i = 1; i <= 12; i++) {
   let num = document.createElement("div");
@@ -10,9 +9,8 @@ for (let i = 1; i <= 12; i++) {
   num.id = "num" + i;
   let numDiv = document.createElement("div");
   num.appendChild(numDiv);
-  numDiv.innerText = i;
-  numDiv.id = "numDiv" + i;
-  $(num).css({
+  numDiv.textContent = i;
+  const numStyle = {
     position: "absolute",
     width: "100%",
     height: "100%",
@@ -21,9 +19,10 @@ for (let i = 1; i <= 12; i++) {
     textAlign: "center",
     fontSize: "32px",
     fontWeight: "700",
-    padding: "18px",
-  });
-  $(num).after({
+    transform: `rotate(${30 * i}deg)`,
+  };
+  Object.assign(num.style, numStyle);
+  const numAfterStyle = {
     content: "",
     position: "absolute",
     top: "0",
@@ -32,33 +31,58 @@ for (let i = 1; i <= 12; i++) {
     width: "5px",
     height: "15px",
     backgroundColor: "black",
-  });
-  $("#num1").css({ transform: "rotate(30deg)" });
-  $("#numDiv1").css({ transform: "rotate(-30deg)" });
-  $("#num2").css({ transform: "rotate(60deg)" });
-  $("#numDiv2").css({ transform: "rotate(-60deg)" });
-  $("#num3").css("transform", "rotate(90deg)");
-  $("#numDiv3").css({ transform: "rotate(-90deg)" });
-  $("#num4").css("transform", "rotate(120deg)");
-  $("#numDiv4").css({ transform: "rotate(-120deg)" });
-  $("#num5").css("transform", "rotate(150deg)");
-  $("#numDiv5").css({ transform: "rotate(-150deg)" });
-  $("#num6").css("transform", "rotate(180deg)");
-  $("#numDiv6").css({ transform: "rotate(-180deg)" });
-  $("#num7").css("transform", "rotate(210deg)");
-  $("#numDiv7").css({ transform: "rotate(-210deg)" });
-  $("#num8").css("transform", "rotate(240deg)");
-  $("#numDiv8").css({ transform: "rotate(-240deg)" });
-  $("#num9").css("transform", "rotate(270deg)");
-  $("#numDiv9").css({ transform: "rotate(-270deg)" });
-  $("#num10").css("transform", "rotate(300deg)");
-  $("#numDiv10").css({ transform: "rotate(-300deg)" });
-  $("#num11").css("transform", "rotate(330deg)");
-  $("#numDiv11").css({ transform: "rotate(-330deg)" });
+  };
+  Object.assign(num.after, numAfterStyle);
+  const numDivStyle = {
+    transform: `rotate(${-30 * i}deg)`,
+  };
+  Object.assign(numDiv.style, numDivStyle);
 }
+//clckFace
+const bodyStyle = {
+  width: "100vw",
+  height: "100vh",
+  overflow: "hidden",
+};
+Object.assign(body.style, bodyStyle);
+const clockStyle = {
+  position: "relative",
+  width: "400px",
+  height: "400px",
+  overflow: "hidden",
+  borderRadius: "50%",
+  border: "5px solid black",
+  backgroundColor: "#fff",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+};
+Object.assign(clock.style, clockStyle);
+const clockAfterStyle = {
+  content: "",
+  position: "absolute",
+  width: "100%",
+  height: "100%",
+  top: 0,
+  left: 0,
+  background:
+    "linear-gradient(285deg, rgba(0, 0, 0, 0.175) 50%, rgba(0, 0, 0, 0.1) 50%)",
+};
+Object.assign(clock.after, clockAfterStyle);
+const clockBeforeStyle = {
+  content: "",
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "20px",
+  height: "20px",
+  backgroundColor: "black",
+  borderRadius: "50%",
+};
+Object.assign(clock.before, clockBeforeStyle);
 
 //drawClock
-
 let handleHour = document.createElement("div");
 clock.appendChild(handleHour);
 handleHour.className = "clockHandle";
@@ -83,82 +107,60 @@ let second = document.createElement("div");
 handleSecond.appendChild(second);
 second.className = "classSecond";
 
-function updateClock() {
-  let date = new Date();
-  let sec = date.getSeconds() / 60;
-  let min = (date.getMinutes() + sec) / 60;
-  let hou = (date.getHours() + min) / 12;
+second.style.width = "1px";
+second.style.height = "40%";
+minute.style.width = "4px";
+minute.style.height = "38%";
+minute.style.borderRadius = "2px";
+hour.style.width = "8px";
+hour.style.height = "28%";
+hour.style.borderRadius = "4px";
 
-  $(handleSecond).css({ transform: "rotate(" + sec * 360 + "deg)" });
-}
-
-updateClock();
-
-$(".clockHandle").css({
+const clockHandleStyle = {
   position: "absolute",
-  top: 0,
-  left: 0,
+  top: "0",
+  left: " 0",
   width: "100%",
   height: "100%",
-});
-$(".classHour, .classMinute, .classSecond").css({
+};
+Object.assign(handleHour.style, clockHandleStyle);
+Object.assign(handleMinute.style, clockHandleStyle);
+Object.assign(handleSecond.style, clockHandleStyle);
+
+const clockClassDivStyle = {
   position: "absolute",
   bottom: "50%",
   left: "50%",
   transform: "translateX(-50%)",
   backgroundColor: "black",
-});
-$(".classSecond").css({
-  width: "1px",
-  height: "40%",
-});
-$(".classMinute").css({
-  width: "4px",
-  height: "38%",
-  borderRadius: "2px",
-});
-$(".classHour").css({
-  width: "8px",
-  height: "28%",
-  borderRadius: "4px",
-});
-//clockCss
-$("*").css({ padding: 0, margin: 0, boxSizing: "border-box" });
-$("body").css({
-  width: "100vw",
-  height: "100vh",
-  overflow: "hidden",
-});
-$(clock).css({
-  position: "relative",
-  width: "400px",
-  height: "400px",
-  overflow: "hidden",
-  borderRadius: "50%",
-  border: "5px solid black",
-  backgroundColor: "#fff",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-});
-$(clock).after({
-  content: "",
-  position: "absolute",
-  width: "100%",
-  height: "100%",
-  top: 0,
-  left: 0,
-  background:
-    "linear-gradient(285deg, rgba(0, 0, 0, 0.175) 50%, rgba(0, 0, 0, 0.1) 50%)",
-});
-$(clock).before({
-  content: "",
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: "20px",
-  height: "20px",
-  backgroundColor: "black",
-  borderRadius: "50%",
-});
+};
+
+Object.assign(hour.style, clockClassDivStyle);
+Object.assign(minute.style, clockClassDivStyle);
+Object.assign(second.style, clockClassDivStyle);
+
+function getTime() {
+  const date = new Date();
+  const seconds = date.getSeconds() / 60;
+  const minutes = (date.getMinutes() + seconds) / 60;
+  const hours = (date.getHours() + minutes) / 12;
+  const getSecStyle = {
+    transform: "rotate(" + seconds * 360 + "deg)",
+  };
+  const getMinStyle = {
+    transform: "rotate(" + minutes * 360 + "deg)",
+  };
+  const getHourStyle = {
+    transform: "rotate(" + hours * 360 + "deg)",
+  };
+  Object.assign(handleHour.style, getHourStyle);
+  Object.assign(handleMinute.style, getMinStyle);
+  Object.assign(handleSecond.style, getSecStyle);
+}
+
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
+}
+
+init();
